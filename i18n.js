9ef5f6b1e4ff6c3747b6c171fc03d52f51c58303
hoangduyartist <hoangduy.artist@gmail.com>
2020-12-21 18:46:22 +0700

    setup multi language with next-i18next

---
+++ i18n.js
@@ -0,0 +1,15 @@
+const NextI18Next = require('next-i18next').default;
+// const { localeSubpaths } = require('next/config').default().publicRuntimeConfig
+const path = require('path');
+
+module.exports = new NextI18Next({
+  defaultLanguage: 'en',
+  otherLanguages: ['vi'],
+  preload: ['en'],
+  fallbackLng: 'en',    
+  browserLanguageDetection: false,
+  serverLanguageDetection: false,
+  // localeSubpaths,
+  localePath: path.resolve('./public/static/locales')
+});
+
--- pages/antdTest.js
+++ pages/antdTest.js
@@ -1,13 +1,48 @@
-import { Row, Col, Button } from "antd";
+import { useContext, useState } from 'react';
+import { Row, Col, Button } from 'antd';
+import { withTranslation, useTranslation } from '../i18n';
 
-export default function AntdTest() {
+function AntdTest({ t, i18n }) {
+  // const { t, i18n } = useTranslation('common');
+  // const [lang, setLang] = useState(i18n.language);
+  const changeLanguage = (lang) => {
+    // setLang(lang);
+    i18n.changeLanguage(lang);
+  };
+
+  console.log('current lang + render', i18n.language);
+
   return (
     <div>
-			<div>antdTest</div>
-      <div><a href="https://ourcodeworld.com/articles/read/497/top-10-best-ui-frameworks-for-reactjs">Top 10 best UI frameworks for reactjs</a></div>
-      <div><a href="https://dev.to/burhanuday/using-ant-design-with-nextjs-custom-variables-for-ant-design-57m5">Using Next + customize var with full styled support</a></div>
-      <div><a href="https://github.com/vercel/next-plugins/issues/598">Compose plugin issue in next js</a></div>
-      <Button type="primary">Primary Button</Button>
+      <div>antdTest</div>
+      <div>
+        <a href="https://ourcodeworld.com/articles/read/497/top-10-best-ui-frameworks-for-reactjs">
+          Top 10 best UI frameworks for reactjs
+        </a>
+      </div>
+      <div>
+        <a href="https://dev.to/burhanuday/using-ant-design-with-nextjs-custom-variables-for-ant-design-57m5">
+          Using Next + customize var with full styled support
+        </a>
+      </div>
+      <div>
+        <a href="https://github.com/vercel/next-plugins/issues/598">
+          Compose plugin issue in next js
+        </a>
+      </div>
+      <div>
+        <a href="https://stackoverflow.com/questions/64822738/next-js-next-i18next-is-always-routing-to-en">
+          Next.js next-i18next is always routing to EN
+        </a>
+      </div>
+      <Button type="primary" onClick={() => changeLanguage('en')}>
+        Endlish
+      </Button>
+      <Button type="primary" onClick={() => changeLanguage('vi')}>
+        Vietnamese
+      </Button>
+      <div>Test language - current language: {i18n.language}</div>
+      <div>{t('test-lang-1')}</div>
       <Row>
         <Col span={24}>col</Col>
       </Row>
@@ -18,3 +53,8 @@ export default function AntdTest() {
     </div>
   );
 }
+
+AntdTest.getInitialProps = () => ({namespacesRequired: ['common']})
+
+// export default AntdTest;
+export default withTranslation('common')(AntdTest);
