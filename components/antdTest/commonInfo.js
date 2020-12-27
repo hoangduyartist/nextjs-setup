78a12dd1bfd77637b110a1bcceaab0fa8958345e
Artist Duy <hoangduy.artist@gmail.com>
2020-12-27 23:22:36 +0700

    keep css built-in support, add plugin that integrate next with antd

--- components/antdTest/commonInfo.js
+++ components/antdTest/commonInfo.js
@@ -2,32 +2,35 @@
 // import { useContext } from 'react';
 import { useTranslation } from "react-i18next";
 import { Card } from "antd";
+import { LinkOutlined } from "@ant-design/icons";
+import styles from './antdTest.module.less';
+
+const links = [
+  { title: "Nextjs and Antd plugin", url: "https://www.npmjs.com/package/next-plugin-antd-less" },
+  { title: "Comparing Styling Methods In Next.js", url: "https://www.smashingmagazine.com/2020/09/comparison-styling-methods-next-js/" },
+  { title: "Top 10 best UI frameworks for reactjs", url: "https://ourcodeworld.com/articles/read/497/top-10-best-ui-frameworks-for-reactjs" },
+  { title: "The best UI frameworks for reactjs 2020", url: "https://www.codeinwp.com/blog/react-ui-component-libraries-frameworks/" },
+  { title: "Using Next + customize var with full styled support", url: "https://dev.to/burhanuday/using-ant-design-with-nextjs-custom-variables-for-ant-design-57m5" },
+  { title: "Compose plugin issue in next js", url: "https://github.com/vercel/next-plugins/issues/598" },
+  { title: "Next.js next-i18next is always routing to EN", url: "https://stackoverflow.com/questions/64822738/next-js-next-i18next-is-always-routing-to-en" },
+]
+
+const LinkDetail = ({ title, url }) => {
+  return (
+    <div className={styles.linkWrapper}>
+      <a href={url}>{title}</a>
+    </div>
+  )
+}
 
 const CommonInfo = () => {
   const { t, i18n } = useTranslation("common");
   // const { i18n: { language } } = useContext(I18nContext);
   return (
     <Card title="Common information">
-      <div>
-        <a href="https://ourcodeworld.com/articles/read/497/top-10-best-ui-frameworks-for-reactjs">
-          Top 10 best UI frameworks for reactjs
-        </a>
-      </div>
-      <div>
-        <a href="https://dev.to/burhanuday/using-ant-design-with-nextjs-custom-variables-for-ant-design-57m5">
-          Using Next + customize var with full styled support
-        </a>
-      </div>
-      <div>
-        <a href="https://github.com/vercel/next-plugins/issues/598">
-          Compose plugin issue in next js
-        </a>
-      </div>
-      <div>
-        <a href="https://stackoverflow.com/questions/64822738/next-js-next-i18next-is-always-routing-to-en">
-          Next.js next-i18next is always routing to EN
-        </a>
-      </div>
+      {links.map((link, index) => (
+        <LinkDetail key={index} title={link.title} url={link.url}/>
+      ))}
     </Card>
   );
 };
--- next.config.js
+++ next.config.js
@@ -1,55 +1,24 @@
 /* eslint-disable */
-const withLess = require("@zeit/next-less");
-const withCSS = require("@zeit/next-css");
-const lessToJS = require("less-vars-to-js");
-const fs = require("fs");
-const path = require("path");
-// const withPlugins = require('next-compose-plugins');
-
-// Where your antd-custom.less file lives
-const themeVariables = lessToJS(
-  fs.readFileSync(path.resolve(__dirname, "./styles/antd-custom.less"), "utf8")
-);
-
-module.exports = withCSS({
-  cssModules: true,
+const withAntdLess = require('next-plugin-antd-less');
+ 
+module.exports = withAntdLess({
+  lessVarsFilePath: './styles/antd-custom.less',
   cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: "[local]___[hash:base64:5]",
+  //   https://github.com/webpack-contrib/css-loader#object
+  //
+  //   sourceMap: true, // default false
+  //   esModule: false, // default false
+  //   modules: {
+  //     exportLocalsConvention: 'asIs',
+  //     exportOnlyLocals: true,
+  //     mode: 'pure',
+  //     getLocalIdent: [Function: getCssModuleLocalIdent]
+  //   }
   },
-  ...withLess({
-    lessLoaderOptions: {
-      // less-loader 6.0
-      lessOptions: {
-        javascriptEnabled: true,
-        modifyVars: themeVariables, // make your antd custom effective
-      },
-      // less-loader 5.0
-      // javascriptEnabled: true,
-      // modifyVars: themeVariables, // make your antd custom effective
-    },
-    webpack: (config, { isServer }) => {
-      if (isServer) {
-        const antStyles = /antd\/.*?\/style.*?/;
-        const origExternals = [...config.externals];
-        config.externals = [
-          (context, request, callback) => {
-            if (request.match(antStyles)) return callback();
-            if (typeof origExternals[0] === "function") {
-              origExternals[0](context, request, callback);
-            } else {
-              callback();
-            }
-          },
-          ...(typeof origExternals[0] === "function" ? [] : origExternals),
-        ];
-
-        config.module.rules.unshift({
-          test: antStyles,
-          use: "null-loader",
-        });
-      }
-      return config;
-    },
-  }),
+  // Other Config Here...
+ 
+  webpack(config) {
+    return config;
+  },
 });
+
